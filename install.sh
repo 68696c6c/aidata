08a926444095890454b269b2db81000d72e6d280
Aaron Hill <68696c6c@gmail.com>
2026-03-31 12:41:01 -0600

    add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+mkdir -p /Users/aaronhill/Code/.claude
+ln -s /Users/aaronhill/Code/aidata/CLAUDE.md /Users/aaronhill/Code/CLAUDE.md || echo "CLAUDE.md exists"
+ln -s /Users/aaronhill/Code/aidata/settings.local.json /Users/aaronhill/Code/.claude/settings.local.json || echo "claude local settings exist"
